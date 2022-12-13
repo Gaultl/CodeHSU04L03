bfa5acf34012cb316834ec1ee91b2e4545d8e9d7
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2022-12-13 09:55:38 -0800

    Created methods to count vowels, remove vowels, and chaeck if a string contains another string

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,63 @@
 public class Main {
+    /*
+    Use the following methods:
+        - .length()
+        - .substring(start, end) doesn't include end
+        - .substring(start) goes to the end of the string
+        - .indexOf(someString)
+        -  someString.equals(otherEquals)
+     */
+
     public static void main(String[] args) {
         System.out.println(VowelCount("This is a string."));
         System.out.println("Correct output: 4");
+        System.out.println(VowelCount("AEIOU"));
+        System.out.println("Correct output: 5");
+        System.out.println(VowelCount("A1234E&^*(^$/IOU"));
+        System.out.println("Correct output: 5");
+        ///////////////////////////////////////////////////////////////////
         System.out.println(VowelRemover("Remove all of the vowels."));
-        System.out.println("Correct output: Rmv ll f th vwls");
+        System.out.println("Correct output: Rmv ll f th vwls.");
+        ///////////////////////////////////////////////////////////////////
         System.out.println(ContainsSubstring("Sentence","ten"));
         System.out.println("Correct output: true");
+        System.out.println(ContainsSubstring("butterfly","butt"));
+        System.out.println("Correct output: true");
+        System.out.println(ContainsSubstring("       "," "));
+        System.out.println("Correct output: true");
+        System.out.println(ContainsSubstring("",""));
+        System.out.println("Correct output: true");
+        ///////////////////////////////////////////////////////////////////
         System.out.println(ReverseString("ABCDEF"));
         System.out.println("Correct output: FEDCBA");
+        ///////////////////////////////////////////////////////////////////
         System.out.println(PalindromeChecker("level"));
         System.out.println("Correct output: true");
+        ///////////////////////////////////////////////////////////////////
     }
 
     /**
-     * Count the vowels in the input string regardless of case
+     * Count the vowels (a,e,i,o, or u) in the input string regardless of case
      * @param input String
      * @return vowel count int
      */
     public static int VowelCount(String input){
+        int length = input.length();
+        int vowels = 0;
 
-        return -1;
+        String lowerInput = input.toLowerCase();
+
+        for (int i = 0; i < length ; i++){
+            String letter = lowerInput.substring(i, i + 1);
+            if (letter.equals("a")
+                    || letter.equals("e")
+                    || letter.equals("i")
+                    || letter.equals("o")
+                    || letter.equals("u")){
+                vowels++;
+            }
+        }
+        return vowels;
     }
 
     /**
@@ -28,8 +66,20 @@ public class Main {
      * @return String with no vowels
      */
     public static String VowelRemover(String input){
+        int length = input.length();
+        String noVowels = "";
 
-        return "";
+        for (int i = 0; i < length ; i++){
+            String letter = input.substring(i, i + 1);
+            if (!(letter.toLowerCase().equals("a")
+                    || letter.toLowerCase().equals("e")
+                    || letter.toLowerCase().equals("i")
+                    || letter.toLowerCase().equals("o")
+                    || letter.toLowerCase().equals("u"))){
+                noVowels = noVowels + letter;
+            }
+        }
+        return noVowels;
     }
 
     /**
@@ -39,7 +89,19 @@ public class Main {
      * @return true if target found, false otherwise
      */
     public static boolean ContainsSubstring(String input, String target){
-
+        //boolean output = input.contains(target);
+        /*if(input.indexOf(target) > 0){
+            return true;
+        }
+        return false;*/
+        int length1 = input.length();
+        int length2 = target.length();
+        for(int i = 0; i <= (length1 - length2); i++){
+            String compare = input.substring(i, i+length2);
+            if(target.equals(compare)){
+                return true;
+            }
+        }
         return false;
     }
 
